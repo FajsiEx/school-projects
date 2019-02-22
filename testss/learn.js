e1d3f69c08a62497d3e0fcaaa2cd5cbdca806e5f
FajsiEx <fajsiexofficial@gmail.com>
2019-02-22 23:25:43 +0100

    random changes

---
+++ testss/learn.js
@@ -0,0 +1,24 @@
+let numbah = 621;
+let stringo = "kghsfgdzjhfgdshjdfgshjdfgshjsdfgfdsgjhdsfgjhk";
+let arrai = [621, "hdjkasghdjsgafjhdgsak", [465545,456465], function(x,y){return x+y}];
+let objekt = {
+    varName: "fdsjndfjsehdsfjhdsfjhdsf",
+    bestAnime: [
+        "Death Note",
+        "That's...",
+        52525252
+    ],
+    funkcioo: function(a,b) {
+        return a-b;
+    }
+};
+const funckdfhsajk = function(q,w) {
+    if(q == w) {
+        return q;
+    }else{
+        return "fhgdsjhi";
+    }
+}
+
+console.log(objekt.funkcioo(25,15));
+console.log(funckdfhsajk(15153,56));
